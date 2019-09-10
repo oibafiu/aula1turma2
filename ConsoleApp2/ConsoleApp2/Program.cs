841f4cc160c00b0c5c48dbf656ea61572d9bafcf
Fabio de Oliveira <faabio.oliver@outlook.com>
2019-09-10 17:24:01 -0300

    COmmit

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,6 +10,31 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            //Texto para consultar o livro
+            Console.Write("Informe o livro a ser consultado:");
+            //Parte do codigo que recebe as informações
+            var numeroDoLivro = Console.ReadLine();
+            //Comparação das informações do livro informado
+            if (numeroDoLivro == "123456")
+            {
+                Console.WriteLine("Livro Indisponível");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Deseja alocar o livro? (1) sim (2) não");
+                var resposta = Console.ReadLine();
+                if (resposta == "1") ;
+                {
+                    Console.WriteLine("Livro alocado");
+                    Console.ReadKey();
+                    return;
+                }
+
+            }
+            
+
         }
     }
 }
