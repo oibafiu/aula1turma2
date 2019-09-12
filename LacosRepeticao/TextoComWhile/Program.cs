58c49e549f4f3ebe02ab815bb3c5d28718a32964
Fabio de Oliveira <faabio.oliver@outlook.com>
2019-09-12 15:57:18 -0300

    Commit

---
+++ LacosRepeticao/TextoComWhile/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextoComWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            var i = 0;
+           
+            var nome = @"Pensando mais a longo prazo, a execução dos pontos do programa estimula a 
+padronização dos modos de operação convencionais. O empenho em analisar a constante divulgação das informações
+acarreta um processo de reformulação e modernização dos relacionamentos verticais entre as hierarquias. No entanto, 
+não podemos esquecer que o fenômeno da Internet faz parte de um processo de gerenciamento dos níveis de motivação 
+departamental. A certificação de metodologias que nos auxiliam a lidar com o entendimento das metas propostas apresenta
+tendências no sentido de aprovar a manutenção do sistema de participação geral. Do mesmo modo,
+o novo modelo estrutural aqui preconizado garante a contribuição de um grupo importante na determinação do fluxo de informações.";
+
+            Console.WriteLine("Encontre a palavra");
+            var palavra = Console.ReadLine();
+
+            while (i < (nome.Length -1));
+            {
+                var palavraParaComparar = nome[i].ToString() +
+                                           nome[i + 1].ToString() +
+                                           nome[i + 2].ToString();
+                                
+
+                if (palavra == palavraParaComparar)
+                    count++;
+
+                i++;
+            }
+
+            Console.WriteLine($"Quantidade total de {palavra} {count}");
+            Console.ReadLine();
+
+            
+
+                
+        }
+
+    }
+    }
+
---
+++ LacosRepeticao/UtilizandoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            while (count < 10000000) 
+            {
+                Console.WriteLine($"Numero:{count}");
+                count++;
+                //+=5 conta de 5 em 5
+            }
+            Console.WriteLine("Terminou o Loop");
+            Console.ReadKey();
+        }
+    }
+}
