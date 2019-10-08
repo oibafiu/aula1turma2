0f6536051ba99276ed03c42aa41aa4bbc11e0708
Fabio de Oliveira <faabio.oliver@outlook.com>
2019-10-08 17:22:15 -0300

    Commit

---
+++ RevisaoEntity/RevisandoEntity/Model/Cerveja.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisandoEntity.Model
+{
+    public class Cerveja
+    {
+        [Key] //Chave primaria, auto incrementada
+        public int Id { get; set; }
+        [StringLength(50)] //QUANTIDADE MAXIMA DE CARACTERES 
+        [Required]
+        public string Nome { get; set; }
+        [StringLength(30)]
+        public string Tipo { get; set; }
+        public double Teor { get; set; }
+    }
+}
